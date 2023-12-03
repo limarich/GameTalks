b5d1c15aa61f4742392b43499b53b31b27037648
Richard <richard.esclima@gmail.com>
2023-12-03 01:02:32 -0300

    feat: list threads

--- discussions/urls.py
+++ discussions/urls.py
@@ -6,6 +6,7 @@ app_name = "discussions"
 urlpatterns = [
     path('teste', views.teste, name="teste"),
     path('posts', views.posts, name="posts"),
+    path('explore', views.explore, name="explore"),
     path('', views.homepage, name="homepage"),
     
 ]
--- discussions/views.py
+++ discussions/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-from .models import Post
+from .models import Post, Thread
 from django.shortcuts import render, redirect
+from django.db.models import Count
 
 def teste(request):
     return HttpResponse("ok")
 
+@login_required
 def posts(request):
     if request.user.is_authenticated:
         posts = Post.objects.filter(user=request.user)
@@ -16,3 +19,14 @@ def posts(request):
 
 def homepage(request):
     return render(request, 'pages/homepage.html')
+
+@login_required
+def explore(request):
+    threads_with_most_posts = Thread.objects.annotate(num_posts=Count('post')).order_by('-num_posts')
+    
+    threads_and_posts = {}
+    for thread in threads_with_most_posts:
+        posts = Post.objects.filter(thread=thread)
+        threads_and_posts[thread] = list(posts)
+    
+    return render(request, 'pages/explore.html', {'threads_and_posts': threads_and_posts})
